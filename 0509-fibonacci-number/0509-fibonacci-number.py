a478c638b0cca7bd98c6bd6f020f8f88e4e0b3c6
Abraham Steve Colina <130194837+steveweenie@users.noreply.github.com>
2025-12-08 12:26:24 -0600

    Time: 41 ms (51.24%), Space: 17.6 MB (63.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,11 @@
 class Solution:
-    memo = {0:0, 1:1}
     def fib(self, n: int) -> int:
-        if n in self.memo:
-            return self.memo[n]
-        self.memo[n] = self.fib(n-2) + self.fib(n-1)
-        return self.memo[n]
+        if n <= 1:
+            return n
+        
+        table = [0, 1]
+        for i in range(2, n + 1):
+            F_n = table[i - 1] + table[i - 2]
+            table.append(F_n)
+        
+        return table[n]
